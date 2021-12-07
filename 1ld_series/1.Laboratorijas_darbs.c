8b5af69a85a6c9ace9249d9976d65b3caa6bb59c
Mārtiņš Opmanis <62936609+Lasiurus@users.noreply.github.com>
2021-12-07 12:16:55 +0200

    update - more content
    Izpildīti gandrīz visi 1ld nosacījumi.

--- 1ld_series/1.Laboratorijas_darbs.c
+++ 1ld_series/1.Laboratorijas_darbs.c
@@ -4,6 +4,9 @@
 
 //1.Laboratorijas darbs
 //Aptuvenās funkcijas vērtības noteikšana ar Teilora rindu.
+//Dotā funkcija - cos(x/2)
+//Definīcijas apgabals D pieder (-inf;+inf)
+//Grafika .png un source code(.gnu file type) atrodami mapē
 
 double cosinus_tailors(double, int);
 void main(){
@@ -20,14 +23,14 @@ void main(){
     scanf("%d",&count);
 
     yy = cosinus_tailors(x,count);
-    printf("Lietotaja funkcija- y=cos(%.6f/2)=%.6f\n",x,yy);
+    printf("\nLietotaja funkcija- y=cos(%.6f/2)=%.6f\n",x,yy);
 
     y=cos(x/2);
     printf("Standarta funkcija cos - y=cos(%.6f/2)=%.6f\n\n",x,y);
 
 
     //ASCII māksla
-    printf("         sum_part_count\n"
+    printf("       sum_part_count\n"
     "         _______\n"
     "         \\             k    2*k\n"
     "          \\        (-1)    x     \n"
@@ -47,16 +50,26 @@ void main(){
 double cosinus_tailors(double x,int sum_part_count){
     double a, S;
     int k =0;
+    double pirmsped_loc, ped_loc;
 
+    printf("NPK\tinput\tloceklis\tsumma\n");
     a = (pow(-1,k)*pow(x,2*k))/(1.);
     S = a;
-    printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
+    printf("%d\t%.3f\t%8.6f\t%8.6f\n",k+1,x,a,S);
 
     while (k<sum_part_count){
         k++;
         a =a * ((-1)*x*x)/((2*k)*(2*k-1)*4);
         S=S+a;
-        printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
+        printf("%d\t%.3f\t%8.6f\t%8.6f\n\n",k+1,x,a,S);
+        if(k==sum_part_count-2){
+            pirmsped_loc = a;
+        }
+        if(k==sum_part_count-1){
+            ped_loc = a;
+        }
     }
+    printf("pirmspeedejais loceklis- %.20f\n",pirmsped_loc);
+    printf("peedejais loceklis- %.20f\n\n",ped_loc);
     return S;
 }
